8e76464bdaad2cdfd26ea0edf67b468f480d7cf0
Ondřej Polách <ondra@ondrejpolach.cz>
2021-09-24 11:07:41 +0200

    portfolio mobile

--- nuxt/pages/portfolio/index.vue
+++ nuxt/pages/portfolio/index.vue
@@ -1,13 +1,12 @@
 <template>
-  <v-container fluid fill-height class="portfolio pa-0 d-flex ">
+  <v-container fluid fill-height class="portfolio pa-0 d-flex">
     <v-container>
       <v-row>
         <v-col>
-          <v-timeline>
+          <v-timeline class="d-none d-md-block">
             <v-timeline-item
               v-for="(project, i) in projects"
               :key="i"
-              small
               :color="project.color"
             >
               <template #opposite>
@@ -24,13 +23,25 @@
                   {{ project.name }}
                 </v-card-title>
                 <v-card-text>
-                  {{
-                    project.description
-                  }}
+                  {{ project.description }}
                 </v-card-text>
               </v-card>
             </v-timeline-item>
           </v-timeline>
+          <v-card class="elevation-2 d-display d-md-none my-10"  v-for="(project, i) in projects" :key="i">
+            <v-img
+              v-if="project.logo"
+              :position="project.position"
+              height="250"
+              :src="project.logo"
+            />
+            <v-card-title class="headline">
+              <span class="font-weight-bold pr-5">{{ project.title }}</span> {{ project.name }}
+            </v-card-title>
+            <v-card-text>
+              {{ project.description }}
+            </v-card-text>
+          </v-card>
         </v-col>
       </v-row>
     </v-container>
@@ -39,61 +50,63 @@
 
 <script>
 export default {
-  data () {
+  data() {
     return {
       projects: [
         {
-          title: '2021 / 01',
-          color: 'success',
-          name: 'Start as freelancer',
-          logo: require('~/assets/hero.png'),
-          position: 'center 30%',
-          description: 'After 4 months of trying out various work as contractor, I decided to take the next step - start own business and go on a freelance journey. So far partly with the current job.'
+          title: "2021 / 01",
+          color: "success",
+          name: "Start as freelancer",
+          logo: require("~/assets/hero.png"),
+          position: "center 30%",
+          description:
+            "After 4 months of trying out various work as contractor, I decided to take the next step - start own business and go on a freelance journey. So far partly with the current job.",
         },
         {
-          title: '2021 / 04',
-          name: 'MJC Zlín ordering and picking system',
-          color: 'success',
-          logo: require('~/assets/velo-mjc-hero.png'),
-          position: 'top center',
+          title: "2021 / 04",
+          name: "MJC Zlín ordering and picking system",
+          color: "success",
+          logo: require("~/assets/velo-mjc-hero.png"),
+          position: "top center",
           description: `I developed multi-step ordering system for sales department of MJC Zlín - leading distributor of hairdressing supplies in Czech republic. 
                                   System also contains module for picking wares from stock based on created orders. Support also generation of invoices for external accounting system.
-                                  From technologic point of view, this system is hosting in Firebase project - cloud functions, firestore, CDN hosting for frontend written in Vue.`
+                                  From technologic point of view, this system is hosting in Firebase project - cloud functions, firestore, CDN hosting for frontend written in Vue.`,
         },
         {
-          title: '2021 / 09',
-          name: 'Website for Patrik Pšeja',
-          color: 'success',
-          logo: require('~/assets/patrik_pseja_hero.png'),
-          position: 'top center',
-          description: `My first freelance web design for the greatest professional photographer I know. The basis of the website is Wordpress with the Novo template. But as usual, it is adjusted as much as possible.`
+          title: "2021 / 09",
+          name: "Website for Patrik Pšeja",
+          color: "success",
+          logo: require("~/assets/patrik_pseja_hero.png"),
+          position: "top center",
+          description: `My first freelance web design for the greatest professional photographer I know. The basis of the website is Wordpress with the Novo template. But as usual, it is adjusted as much as possible.`,
         },
         {
-          title: '2021 / 10',
-          color: 'primary',
-          name: 'Work in progress',
-          description: 'Currently working on some interesting projects. One project going to last phase, I think I can be more opened, two letters, MH. Stay tuned.'
-        }
-      ]
-    }
+          title: "2021 / 10",
+          color: "primary",
+          name: "Work in progress",
+          description:
+            "Currently working on some interesting projects. One project going to last phase, I think I can be more opened, two letters, MH. Stay tuned.",
+        },
+      ],
+    };
   },
-  head () {
+  head() {
     return {
-      title: 'Portfolio',
+      title: "Portfolio",
       meta: [
         {
-          hid: 'description',
-          name: 'description',
-          content: 'Ondřej Polách | Portfolio'
-        }
-      ]
-    }
-  }
-}
+          hid: "description",
+          name: "description",
+          content: "Ondřej Polách | Portfolio",
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style >
 .portfolio {
-    background: #fff;
+  background: #fff;
 }
 </style>
